0d4e4f822db8f2f71e28b155fd91fc214923ada5
Chaima <chaima-aicheche@laplateforme.io>
2022-02-25 10:04:55 +0100

    page a propos a finir

---
+++ apropos.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="UTF-8">
+        <title>A propos </title>
+        <link rel="stylesheet" href="apropos.css">
+ </head>
+<body>
+
+<div class="polaroid">
+    <div class="image-container">
+  <img src="img/jasmine.JPG" alt="Jasmine" style="width:30%">
+  <img class="transition-img" src="img/jasmine1.png" alt="Jasmine1">
+  </div>
+  <div class="container">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Mauris vulputate laoreet urna. Integer magna. 
+  Donec facilisis lectus sed quam. 
+  Curabitur sit amet lacus id lacus facilisis venenatis. </p>
+  </div>
+</div>
+
+<div class="polaroid">
+  <img src="lights600x400.jpg" alt="Badr" style="width:100%">
+  <div class="container">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Mauris vulputate laoreet urna. Integer magna. 
+  Donec facilisis lectus sed quam. 
+  Curabitur sit amet lacus id lacus facilisis venenatis.</p>
+  </div>
+</div>
+
+<div class="polaroid">
+  <img src="lights600x400.jpg" alt="Chaima" style="width:100%">
+  <div class="container">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Mauris vulputate laoreet urna. Integer magna. 
+  Donec facilisis lectus sed quam. 
+  Curabitur sit amet lacus id lacus facilisis venenatis.</p>
+  </div>
+</div>
+
+</body>
+</html>
