36e3a26a072a79a1c495add242485bfdf8708372
jasmine-messaadi <91733927+jasmine-messaadi@users.noreply.github.com>
2022-04-05 16:36:14 +0200

    INDEX 2 IMPORTANT

---
+++ index2.php
@@ -0,0 +1,123 @@
+
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>CSS Glitch Effect | Demo 2 | Codrops</title>
+		<meta name="description" content="A glitch effect using CSS animations and clip-path" />
+		<meta name="keywords" content="glitch, effect, css, animations" />
+		<meta name="author" content="Codrops" />
+		<link href="https://fonts.googleapis.com/css?family=Bungee" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="CSSGlitchEffect-master/css/base.css"/>
+		<link rel="stylesheet" type="text/css" href="CSSGlitchEffect-master/css/demo2.css"/>
+		<script>document.documentElement.className="js";var supportsCssVars=function(){var e,t=document.createElement("style");return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};supportsCssVars()||alert("Please view this demo in a modern browser that supports CSS Variables.");</script>
+		</head>
+			
+     
+	<body class="demo-2 loading">
+		
+		<?php include 'header.php'	?>
+
+		<audio autoplay loop>
+			
+			<source src="/img/BLACK OPS 2 ZOMBIES_ OFFICIAL Theme Song.wav" type="audio/wav"></audio>
+	<header> 
+			<div class="content">
+				<div class="glitch">
+					<div class="glitch__img"></div>
+					<div class="glitch__img"></div>
+					<div class="glitch__img"></div>
+					<div class="glitch__img"></div>
+					<div class="glitch__img"></div>
+				</div>
+				<h2 class="content__title">총을 쏘다</h2>
+			</div>
+	
+	</header>
+		<script src="CSSGlitchEffect-master/js/imagesloaded.pkgd.min.js"></script>
+		<script src="CSSGlitchEffect-master/js/demo.js"></script>
+		
+<!-- section service -->
+
+	<section class="services">
+			<div class="service-items">
+				<img src="img/encryption.svg" style="width: 60%; padding:5%;margin: 10%">
+					<p class="service-details">Vos payments sont anonyme et intraçable</p>
+			</div>
+			<div class="service-items">
+				<img src="img/convenience.svg" style="width: 60%; padding: 5%;margin: 10%">
+					<p class="service-details">Expérience utilisateur familière et pratique</p>
+			</div>
+			<div class="service-items">
+				<img src="img/self-custodial.svg" style="width: 60%; padding: 5%;margin: 10%">
+					<p class="service-details">Auto-conservation : vos clés, votre crypto</p>
+			</div>
+	</section>
+<!-- fin section -->
+
+<!-- section NEW -->
+
+	<section class="best-items">
+			<h2 class="section-title">Newest Collections Added</h2>
+			
+<!-- carousel -->
+			<div class="carousel-wrapper">
+				<span id="item-1"></span>
+				<span id="item-2"></span>
+				<span id="item-3"></span>
+					<div class="carousel-item item-1">
+				  		<a href="#item-3" class="arrow-prev arrow"></a>
+				 		<a href="#item-2" class="arrow-next arrow"></a>
+					</div>
+					<div class="carousel-item item-2"></a>
+				 		 <a href="#item-1" class="arrow-prev arrow"></a>
+				 		 <a href="#item-3" class="arrow-next arrow"></a>
+					</div>
+					<div class="carousel-item item-3">
+				  		<a href="#item-2" class="arrow-prev arrow"></a>
+				 		<a href="#item-1" class="arrow-next arrow"></a>
+					</div>
+			</div>	  
+	</section>
+<!-- fin section -->
+
+<!-- section tout les nfts -->
+	<section class="new-nfts">
+		<h2 class="section-title">Top Collections</h2>
+		<div class="nft-grid">
+			<a class="nft-grid1 nft-box">
+				<div class="nft-details">
+					<p class="nft-name">Grenart</p>
+					<p class="nft-price">5 ETH</p>
+				</div>
+			</a>
+			<a class="nft-grid2 nft-box">
+				<div class="nft-details">
+					<p class="nft-name">Grenart</p>
+					<p class="nft-price">5 ETH</p>	
+				</div>
+			</a>			
+			<a class="nft-grid3 nft-box">
+				<div class="nft-details">
+					<p class="nft-name">Grenart</p>
+					<p class="nft-price">5 ETH</p>
+				</div>
+			</a>
+	</section>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	<p>ejz</p>
+	</body>
+</html>
+
